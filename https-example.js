e1400e18eca8d3cc5d98352b841c53e2a2f9e862
David Grant <mrdavidgrant@outlook.com>
2017-09-10 19:41:36 +0000

    succesfully made request

--- https-example.js
+++ https-example.js
@@ -1,3 +1,15 @@
 var https = require('https')
+var options = {
+  host: 'www.example.org',
+  path: '/'
+}
 
-console.log('required')
+var callback = function() {
+  console.log('In response handler callback!')
+}
+
+console.log("I'm about to make the request")
+
+https.request(options, callback).end()
+
+console.log("I've made the request")
